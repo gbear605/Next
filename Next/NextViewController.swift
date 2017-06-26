da1d4bf42aed8f6da6f931a253f7fca09273dc85
gbear605 <garrisontaylor@gmail.com>
2017-06-26 16:22:11 -0400

    Add button to show first existing todo

--- Next/NextViewController.swift
+++ Next/NextViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class NextViewController: UIViewController {
 
+    @IBOutlet weak var nextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -12,6 +14,18 @@ class NextViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func next(_ sender: UIButton) {
+        var flagTodosToPickFrom = false
+        for category in TodoModel.Category.list {
+            if TodoModel.numTodos(category: category) > 0 {
+                flagTodosToPickFrom = true
+                nextLabel.text = TodoModel.get(category, from: 0).name
+            }
+        }
+        if !flagTodosToPickFrom {
+            nextLabel.text = "Make a todo!"
+        }
+    }
+    
 }
 
